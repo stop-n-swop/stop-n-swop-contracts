a4ed36056a98cc8f045b0b310610011c5467a988
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-04-06 21:31:51 +0100

    feat: initial development

---
+++ src/user/api.ts
@@ -0,0 +1,25 @@
+import type { OauthProvider } from "./enums";
+import type { User } from "./entities";
+
+export interface RefreshTokenRequest {
+  token: string;
+}
+export interface RefreshTokenResponse {
+  authToken: string;
+  refreshToken: string;
+  userId: string;
+}
+
+export interface LoginRequest {
+  provider: OauthProvider;
+  token: string;
+}
+export type LoginResponse = RefreshTokenResponse;
+
+export interface UpdateUserRequest
+  extends Partial<Omit<User, "address" | "preferences">> {
+  address?: Partial<User["address"]>;
+  preferences?: Partial<User["preferences"]>;
+}
+
+export type UpdateUserResponse = void;
