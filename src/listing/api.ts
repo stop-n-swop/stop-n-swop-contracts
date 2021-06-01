0674596ae272678b82a416135da6640e52d9358f
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-06-01 10:03:22 +0100

    chore: update contracts

--- src/listing/api.ts
+++ src/listing/api.ts
@@ -10,21 +10,15 @@ export interface GetListingRequirementsResponse {
   }>;
 }
 
-export type CreateListingRequest = Omit<
-  Listing,
-  "id" | "createdDate" | "username" | "location" | "rating" | "status"
->;
+export type CreateListingRequest = Omit<UpdateListingRequest, "id">;
 
 export interface CreateListingResponse {
   id: string;
 }
 
-export interface UpdateListingParams {
-  listingId: string;
-}
 export type UpdateListingRequest = Omit<
   Listing,
-  "createdDate" | "username" | "location" | "rating" | "status" | "id"
+  "createdDate" | "username" | "location" | "rating" | "status"
 >;
 export type UpdateListingResponse = Listing;
 
