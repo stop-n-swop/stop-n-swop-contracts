6e80df259b2d23d1a061df9eb1b337effb446a62
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-06-01 10:03:22 +0100

    chore: update contracts

--- .yalc/@sns/contracts/listing/api.d.ts
+++ .yalc/@sns/contracts/listing/api.d.ts
@@ -2,56 +2,56 @@ import { AuditItem, Listing } from "./entities";
 import { Condition, Region } from "./enums";
 export declare type GetListingRequirementsRequest = void;
 export interface GetListingRequirementsResponse {
-    photos: Array<{
-        key: string;
-        required: boolean;
-    }>;
+  photos: Array<{
+    key: string;
+    required: boolean;
+  }>;
 }
-export declare type CreateListingRequest = Omit<Listing, "id" | "createdDate" | "username" | "location" | "rating" | "status">;
+export declare type CreateListingRequest = Omit<UpdateListingRequest, "id">;
 export interface CreateListingResponse {
-    id: string;
+  id: string;
 }
-export interface UpdateListingParams {
-    listingId: string;
-}
-export declare type UpdateListingRequest = Omit<Listing, "createdDate" | "username" | "location" | "rating" | "status" | "id">;
+export declare type UpdateListingRequest = Omit<
+  Listing,
+  "createdDate" | "username" | "location" | "rating" | "status"
+>;
 export declare type UpdateListingResponse = Listing;
 export interface SearchListingsRequest {
-    productId?: string;
-    platformId?: string;
-    boxed?: boolean;
-    instructions?: boolean;
-    condition?: Condition | Condition[];
-    region?: Region | Region[];
-    rating?: number;
-    minPrice?: number;
-    maxPrice?: number;
+  productId?: string;
+  platformId?: string;
+  boxed?: boolean;
+  instructions?: boolean;
+  condition?: Condition | Condition[];
+  region?: Region | Region[];
+  rating?: number;
+  minPrice?: number;
+  maxPrice?: number;
 }
 export interface SearchListingsResponse {
-    listings: Listing[];
+  listings: Listing[];
 }
 export interface GetListingParams {
-    listingId: string;
+  listingId: string;
 }
 export declare type GetListingRequest = void;
 export declare type GetListingResponse = Listing;
 export interface GetProductsListingCountRequest {
-    products: Array<{
-        productId: string;
-        platformId: string;
-    }>;
+  products: Array<{
+    productId: string;
+    platformId: string;
+  }>;
 }
 export interface GetProductsListingCountResponse {
-    counts: Array<{
-        productId: string;
-        platformId: string;
-        count: number;
-    }>;
+  counts: Array<{
+    productId: string;
+    platformId: string;
+    count: number;
+  }>;
 }
 export interface GetHistoryParams {
-    listingId: string;
+  listingId: string;
 }
 export declare type GetHistoryRequest = void;
 export interface GetHistoryResponse {
-    history: AuditItem[];
+  history: AuditItem[];
 }
