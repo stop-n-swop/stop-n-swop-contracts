99718e2e56afe89dac13e94c118e6a7426aaddfb
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-06-21 10:01:58 +0100

    feat: show address to post to

--- .yalc/@sns/contracts/basket/enums.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum Status {
-    LOCAL_ONLY = "localOnly",
-    IN_PROGRESS = "inProgress",
-    COMPLETED = "completed",
-    ABORTED = "aborted"
-}
--- .yalc/@sns/contracts/src/basket/api.ts
+++
@@ -1,9 +0,0 @@
-import { Basket } from "./entities";
-
-export type FetchBasketRequest = void;
-export type FetchBasketResponse = Basket;
-
-export interface AddToBasketRequest {
-  listingId: string;
-}
-export type AddToBasketResponse = {};
