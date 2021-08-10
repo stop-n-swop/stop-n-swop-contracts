e3d38549faa9db9c05e48fa1f965b146713e6f8a
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-08-10 10:04:30 +0100

    feat: paypal integration

--- src/listing/logic.ts
+++ src/listing/logic.ts
@@ -1,11 +1,3 @@
-import {
-  BASE_CHARGE_RATE,
-  PROTECTION_RATE,
-  PROVIDER_BASE_COST,
-  PROVIDER_BASE_RATE,
-  PROVIDER_PAYOUT_COST,
-  SCALE_CHARGE_LIMIT,
-} from "./constants";
 import { Listing } from "./entities";
 
 /** Returns the base price of the listing, not including postage */
@@ -25,12 +17,12 @@ export const getListedPrice = (listing: Listing) => {
 
 /** Returns the amount of order protection that will be deducted from the listed price */
 export const getProtectionCharge = (listing: Listing) => {
-  return getListedPrice(listing) * PROTECTION_RATE;
+  return Math.ceil(getListedPrice(listing) * 0.05);
 };
 
-/** Returns the platform charge that will be deducted from the listed price */
+/** Returns the total platform charge that will be deducted from the listed price */
 export const getPlatformCharge = (listing: Listing) => {
-  return getListedPrice(listing) * BASE_CHARGE_RATE;
+  return Math.ceil(getListedPrice(listing) * 0.04) + 30;
 };
 
 /** Returns the actual price the customer will pay i.e. price + postage */
@@ -43,27 +35,9 @@ export const getDisplayPrice = (listing: Listing) => {
   return getBasePrice(listing);
 };
 
-/** Returns the amount we expect the payment provider to charge */
-export const getProviderCharges = (listing: Listing) => {
-  const price = getFinalPrice(listing);
-  const cut = price * PROVIDER_BASE_RATE;
-  return cut + PROVIDER_BASE_COST + PROVIDER_PAYOUT_COST;
-};
-
 /** Returns the amount sns will charge the seller (i.e. order protection + platform charge) */
 export const getListingCharges = (listing: Listing) => {
-  const protection = getProtectionCharge(listing);
-  const platform = getPlatformCharge(listing);
-  const provider = getProviderCharges(listing);
-
-  let charge = protection + platform;
-  const diff = charge - provider;
-
-  if (diff < SCALE_CHARGE_LIMIT) {
-    charge = SCALE_CHARGE_LIMIT - diff;
-  }
-
-  return charge;
+  return getProtectionCharge(listing) + getPlatformCharge(listing);
 };
 
 /** Returns the amount the seller will receive for a listing */
@@ -74,6 +48,27 @@ export const getListingProfit = (listing: Listing) => {
 /** Returns the total cut sns will take from the buyer and the seller */
 export const getTotalCharges = (listing: Listing) => {
   return getListingCharges(listing);
+};
+
+// These are all just speculative of course
+/** The amount paypal charges on pay in */
+export const getProviderPayInCharge = (listing: Listing) => {
+  return Math.ceil(getFinalPrice(listing) * 0.029) + 30;
+};
+
+/** Calculates the payout charge of any amount */
+export const calculateProviderPayOutCharge = (amount: number) => {
+  return Math.ceil(amount * 0.02);
+};
+
+/** the amount paypal charges on pay out */
+export const getProviderPayOutCharge = (listing: Listing) => {
+  return calculateProviderPayOutCharge(getListingProfit(listing));
+};
+
+/** Returns the amount we expect the payment provider to charge */
+export const getProviderCharges = (listing: Listing) => {
+  return getProviderPayInCharge(listing) + getProviderPayOutCharge(listing);
 };
 
 /** Returns the amount sns will have after the payment provider has taken its cut */
