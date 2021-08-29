05e5faad515992aa659f60c62b2a8e8afb53eb38
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2021-08-29 11:13:56 +0100

    feat: sort and limit listings

--- src/listing/api.ts
+++ src/listing/api.ts
@@ -35,6 +35,8 @@ export interface SearchListingsRequest {
   maxPrice?: number;
   status?: Status;
   username?: string;
+  sortBy?: string;
+  limit?: number;
 }
 export interface SearchListingsResponse {
   listings: Listing[];
