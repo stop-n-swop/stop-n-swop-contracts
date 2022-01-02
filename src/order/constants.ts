b38201d47bc6f066cb7d442bc969fe8f894c8f17
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2022-01-02 20:35:54 +0000

    feat: add order consts

---
+++ src/order/constants.ts
@@ -0,0 +1,3 @@
+export const LAPSED_DAYS_THRESHOLD = 10;
+
+export const LAPSED_DAYS_WARNING = 7;
--- src/order/index.ts
+++ src/order/index.ts
@@ -1,3 +1,4 @@
 export * from './api';
+export * from './constants';
 export * from './entities';
 export * from './enums';
